2985224853e29d20be723591302c409418b96c43
Piotr Pacuk <piotrek.pacuk@gmail.com>
2023-07-23 22:33:18 +0200

    added
    SharedLayout css, refresh user

--- src/components/Pages/ContactsPage.jsx
+++ src/components/Pages/ContactsPage.jsx
@@ -2,8 +2,17 @@ import { ContactForm } from "components/ContactForm/ContactForm";
 import { ContactList } from "components/ContactList/ContactList";
 import { Filter } from "components/Filter/Filter";
 import Section from "components/Section/Section";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchContacts } from "redux/operations/contacts";
 
 export const ContactsPage = () => {
+ const dispatch = useDispatch();
+
+ useEffect(() => {
+   dispatch(fetchContacts());
+ }, [dispatch]);
+
   return (
     <>
       <Section title="Phonebook">
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,21 +1,32 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, Outlet } from 'react-router-dom';
 import { logOut } from 'redux/operations/auth';
-import { selectAuthToken } from 'redux/selectors/auth';
+import { selectAuthToken, selectAuthUser } from 'redux/selectors/auth';
+import css from './SharedLayout.module.css';
 
 export const SharedLayout = () => {
   const token = useSelector(selectAuthToken);
+  const user = useSelector(selectAuthUser);
   const dispatch = useDispatch();
-  
-
+    console.log(user);
+    
   return (
     <>
-      <header>
-        <nav>
+      <header className={css.headerContainer}>
+        <img
+          src="https://assets.dryicons.com/uploads/icon/svg/1512/phone_book.svg"
+          width="50px"
+          alt="Logo-phonebook"
+        />
+        <h2>Phonebook</h2>
+        <nav className={css.navContainer}>
           {token ? (
             <>
               <Link to="/">Home</Link>
-              <button onClick={() => dispatch(logOut())}>Logout</button>
+              <p>{user.name}</p>
+              <button type="button" onClick={() => dispatch(logOut())}>
+                Logout
+              </button>
             </>
           ) : (
             <>
