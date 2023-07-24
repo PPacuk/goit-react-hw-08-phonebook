7c4f8aaddd8f939b687187096015c7ca1abcfe8b
Piotr Pacuk <piotrek.pacuk@gmail.com>
2023-07-24 20:55:36 +0200

    fix
    by remove react-typed

--- src/components/Pages/HomePage.jsx
+++ src/components/Pages/HomePage.jsx
@@ -1,12 +1,10 @@
-import ReactTyped from 'react-typed';
 import css from './HomePage.module.css';
 
 export const Home = () => {
   return (
     <div className={css.homeContainer}>
       <div className={css.homeBox}>
-        <ReactTyped strings={['Welcome on phonebook, please login or register to create your own list!']} typeSpeed={50} loop={false} />
-        
+        Welcome on phonebook, please login or register to create your own list!
       </div>
     </div>
   );
