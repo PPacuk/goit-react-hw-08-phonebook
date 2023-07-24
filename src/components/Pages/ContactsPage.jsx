16ed9190d5cb4cad62ba0acf717e89f0a5c1703e
Piotr Pacuk <piotrek.pacuk@gmail.com>
2023-07-24 10:54:17 +0200

    Update ContactsPage.jsx
    fix

--- src/components/Pages/ContactsPage.jsx
+++ src/components/Pages/ContactsPage.jsx
@@ -2,17 +2,9 @@ import { ContactForm } from "components/ContactForm/ContactForm";
 import { ContactList } from "components/ContactList/ContactList";
 import { Filter } from "components/Filter/Filter";
 import Section from "components/Section/Section";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { refreshUser } from "redux/operations/auth";
-import { fetchContacts } from "redux/operations/contacts";
+
 
 export const ContactsPage = () => {
- const dispatch = useDispatch();
-
-//  useEffect(() => {
-//    dispatch(fetchContacts());
-//  }, [dispatch]);
 
   return (
     <>
